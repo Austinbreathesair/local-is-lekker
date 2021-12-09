64c351d8d0c4b1db540137b126f527f4139b78a9
Austinbreathesair <austinbreathesair@gmail.com>
2021-12-09 21:39:29 +0200

    Initial Commit

---
+++ houses.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    picture: '/img/House1.jpg',
+    type: 'Entire House',
+    town: 'Vanderbijlpark',
+    title: 'Zeekoe Lodge Luxury Tent House.'
+  },
+  {
+    picture: '/img/House2.jpg',
+    type: 'Entire House',
+    town: 'Vanderbijlpark',
+    title: 'Pont de Val Self-Catering Riverview Dragonfly B9'
+  }
+]
