c43309fe4c93e3d8fed15f2c3642148350bf86c6
Austinbreathesair <austinbreathesair@gmail.com>
2021-12-12 11:55:38 +0200

    feat: added details function

--- houses.js
+++ houses.js
@@ -1,14 +1,20 @@
 export default [
   {
+    id: 1,
     picture: '/img/House1.jpg',
     type: 'Entire House',
     town: 'Vanderbijlpark',
-    title: 'Zeekoe Lodge Luxury Tent House.'
+    title: 'Zeekoe Lodge Luxury Tent House.',
+    description: 'Our luxurious self catering tent is perfect for a weekend breakaway. The unit is fitted with a queen size bed and basic amenities. The tent has a quaint kitchenette, with basic appliances. The cottage has a shower, toilet & vanity. The spacious patio is the perfect setting for a morning coffee or perfect sundowner.',
+    guests: 2
   },
   {
+    id: 2,
     picture: '/img/House2.jpg',
     type: 'Entire House',
     town: 'Vanderbijlpark',
-    title: 'Pont de Val Self-Catering Riverview Dragonfly B9'
+    title: 'Pont de Val Self-Catering Riverview Dragonfly B9',
+    description: 'Dragonfly Luxury Riverview Self-Catering Rooms are situated on the Vaal River at the exquisite Pont de Val just an hour from Joburg.From your front door you have access to Pont de Val’s stunning Restaurant 1min walk, Luxury Spa 1min walk, Free Entrance to the Winery/Picnic park 5min walk & Village shops 5min walk.Dragonfly VIP is also the proud sole operators of Luxury Boat Cruises and Retro Sidecar Outrides, ensuring a unique and memorable experience!',
+    guests: 2
   }
 ]
